bb42884fe06ec209fe77791e597a47a9b73fce12
yeon <rk_yeon@naver.com>
2025-08-10 23:45:26 +0900

    🍉: 1766번 / 문제집 / 🥇2

---
+++ Baekjoon/1766.py
@@ -0,0 +1,32 @@
+# 우선순위 큐
+import sys
+import heapq
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+in_degree = [0] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+
+    in_degree[b] += 1
+    graph[a].append(b)
+
+queue = []
+
+for i in range(1, n + 1):
+    if in_degree[i] == 0:
+        heapq.heappush(queue, i)
+
+result = []
+while queue:
+    current = heapq.heappop(queue)
+    result.append(current)
+
+    for g in graph[current]:
+        in_degree[g] -= 1
+
+        if in_degree[g] == 0:
+            heapq.heappush(queue, g)
+print(*result)
