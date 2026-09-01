2dfcbf446424e6c4465c48aab4b0c6c758fb53eb
mymemo1508 <mymemo1508@gmail.com>
2026-09-01 11:50:01 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.08 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String my_string, String target) {
+        if (my_string.indexOf(target) != -1) {
+            return 1;
+        } else  {
+            return 0;
+        }
+    }
+}
